4a19cd2f3ffc29a581e412aba1e76869fb168f5d
PabloGonRod <pabloyquemas2@gmail.com>
2023-11-06 17:57:43 +0100

    Finish with fragment

--- app/src/main/java/com/pgonrod/superheroes/presentation/SuperHeroesDetailFragment.kt
+++ app/src/main/java/com/pgonrod/superheroes/presentation/SuperHeroesDetailFragment.kt
@@ -1,28 +1,35 @@
 package com.pgonrod.superheroes.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.pgonrod.app.extensions.loadurl
 import com.pgonrod.superheroes.data.ApiClient
 import com.pgonrod.superheroes.data.biography.BiographyDataRepository
 import com.pgonrod.superheroes.data.biography.local.XmlBiographyLocalDataSource
-import com.pgonrod.superheroes.data.superhero.SuperheroesDataRepository
 import com.pgonrod.superheroes.data.biography.remote.api.BiographyApiRemoteDataSource
 import com.pgonrod.superheroes.data.powerstats.PowerStatsDataRepository
 import com.pgonrod.superheroes.data.powerstats.local.XmlPowerStatsLocalDataSource
 import com.pgonrod.superheroes.data.powerstats.remote.api.PowerStatsApiRemoteDataSource
+import com.pgonrod.superheroes.data.superhero.SuperheroesDataRepository
 import com.pgonrod.superheroes.data.superhero.local.XmlSuperHeroLocalDataSource
 import com.pgonrod.superheroes.data.superhero.remote.api.SuperHeroApiRemoteDataSource
-import com.pgonrod.superheroes.databinding.ActivitySuperHeroesDetailBinding
+import com.pgonrod.superheroes.databinding.FragmentSuperHeroesDetailBinding
 import com.pgonrod.superheroes.domain.GetSuperHeroUseCase
 import com.pgonrod.superheroes.presentation.adapter.SuperHeroImageDetailAdapter
 
-class SuperHeroesDetailActivity : AppCompatActivity() {
+class SuperHeroesDetailFragment : Fragment() {
 
-    private lateinit var binding: ActivitySuperHeroesDetailBinding
+    private var _binding: FragmentSuperHeroesDetailBinding? = null
+    private val binding get() = _binding!!
 
     private val detailAdapter = SuperHeroImageDetailAdapter()
 
@@ -31,36 +38,46 @@ class SuperHeroesDetailActivity : AppCompatActivity() {
         SuperHeroesDetailViewModel(
             GetSuperHeroUseCase(
                 SuperheroesDataRepository(
-                    XmlSuperHeroLocalDataSource(getSharedPreferences("Superheroes", MODE_PRIVATE)),
+                    XmlSuperHeroLocalDataSource(requireContext().getSharedPreferences("Superheroes", MODE_PRIVATE)),
                     SuperHeroApiRemoteDataSource(ApiClient())
                 ),
                 BiographyDataRepository(
-                    XmlBiographyLocalDataSource(getSharedPreferences("Biography", MODE_PRIVATE)),
+                    XmlBiographyLocalDataSource(requireContext().getSharedPreferences("Biography", MODE_PRIVATE)),
                     BiographyApiRemoteDataSource(ApiClient())
                 ),
                 PowerStatsDataRepository(
-                    XmlPowerStatsLocalDataSource(getSharedPreferences("PowerStats", MODE_PRIVATE)),
+                    XmlPowerStatsLocalDataSource(requireContext().getSharedPreferences("PowerStats", MODE_PRIVATE)),
                     PowerStatsApiRemoteDataSource(ApiClient())
                 )
 
             )
         )
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivitySuperHeroesDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setupObserver()
-        val id = intent.extras!!.getInt("id")
-        viewModel.loadSuperHeroesDetail(id)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSuperHeroesDetailBinding.inflate(inflater, container, false)
         setupView()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObserver()
+        setFragmentResultListener("requestKey"){requestKey, bundle ->
+            val result = bundle.getInt("bundleKey")
+            viewModel.loadSuperHeroesDetail(result)
+        }
     }
 
     fun setupView(){
         binding.apply {
             imagelist.apply {
                 layoutManager = LinearLayoutManager(
-                    this@SuperHeroesDetailActivity,
+                    requireContext(),
                     LinearLayoutManager.HORIZONTAL,
                     false
                 )
@@ -77,7 +94,7 @@ class SuperHeroesDetailActivity : AppCompatActivity() {
             Log.d("@dev", "ID: $detail")
         }
 
-        viewModel.uidetailState.observe(this, observerdetail)
+        viewModel.uidetailState.observe(viewLifecycleOwner, observerdetail)
     }
 
 
@@ -92,5 +109,9 @@ class SuperHeroesDetailActivity : AppCompatActivity() {
             detailAdapter.setDataItems(detail.images)
         }
 
+    }
+
+    companion object {
+        fun newInstance()= SuperHeroesDetailFragment()
     }
 }
